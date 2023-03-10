1937d5866809391a761249d11f58fe649213aa51
Gustavo <gugu01gugu@gmail.com>
2023-03-10 16:33:39 -0300

    feat: additing function tabulate

--- Python OO/AUVs.py
+++ Python OO/AUVs.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 class AUVs():
     lista_auvs = []
     def __init__(self, nome, numero_thursters, ano_construção, numero_cameras, sensores):
@@ -8,21 +9,19 @@ class AUVs():
         self.ano_construção = ano_construção
         
 
-    def exibir(self):
+    def exibir_individualmente(self):
         print(f'Nome do AUV: {self.nome}, possui thursters: {self.numero_thursters} e foi construído em {self.ano_construção}. Atualmente possui {self.numero_cameras} câmeras.')
         print(f'Possui os seguintes sensores: {self.lista_sensores}')
         
     def catalogar_auv(*auvs):
         AUVs.lista_auvs.extend(auvs)
 
-    def listar_auvs():
-        print()
+    def listar_tabela_auvs():
+        lista = []
         for auv in AUVs.lista_auvs:
-            print(f'Nome do AUV: {auv.nome}, possui thursters: {auv.numero_thursters} e foi construído em {auv.ano_construção}. Atualmente possui {auv.numero_cameras} câmeras.')
-            print(f'Possui os seguintes sensores: {auv.lista_sensores}')
-        print()
- 
-
+            lista.append([auv.nome, auv.numero_thursters, auv.ano_construção, auv.numero_cameras, auv.lista_sensores])
+        print(tabulate(lista, headers=['Nome', 'Thursters', 'Ano de construção', 'Câmeras', 'Sensores']))
+        
     def definir_antiguidade():
         idadeMaior = 0
         lista = []
@@ -43,11 +42,14 @@ class AUVs():
                 numeroCamera = auv.numero_cameras
         print(f'O AUV com maior numero de cameras é o {nome_auv}')
 
-                    
-auv1 = AUVs('Gustavo', 1, 2, 3, ['Oi', 'eu'])
-auv2 = AUVs('Marcelo', 2, 3, 4, ['h', 'l'])
-AUVs.catalogar_auv(auv1, auv2)
-AUVs.listar_auvs()
-AUVs.definir_antiguidade()
-AUVs.maior_angulo()
+
+#Exemplos aleatórios                   
+auv1 = AUVs('Lua', 1, 2, 4, ['Oi', 'eu'])
+auv2 = AUVs('HueBr', 2, 1, 3, ['h', 'l'])
+
+# AUVs.catalogar_auv(auv1, auv2)
+# AUVs.definir_antiguidade()
+# AUVs.maior_angulo()
+# AUVs.listar_tabela_auvs()
+#auv1.exibir_individualmente()
 
